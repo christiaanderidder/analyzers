2dfa4665ab20c57b540c9cea7773da25ca3ad070
Christiaan de Ridder <christiaan@channelengine.com>
2026-04-11 13:26:23 +0200

    Replace async assert with basic Assert.Fail to add custom message with context

--- tests/Analyzers.Tests/Helpers/TUnitVerifier.cs
+++ tests/Analyzers.Tests/Helpers/TUnitVerifier.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Testing;
@@ -6,46 +7,81 @@ namespace Analyzers.Tests.Helpers;
 
 internal sealed class TUnitVerifier : IVerifier
 {
-    public void Empty<T>(string collectionName, IEnumerable<T> collection) =>
-        AsyncHelper.RunSync(async () => await Assert.That(collection).IsEmpty());
+    private readonly ImmutableStack<string> _context;
 
-    public void Equal<T>(T expected, T actual, string? message = null) =>
-        AsyncHelper.RunSync(async () => await Assert.That(actual).IsEqualTo(expected));
+    public TUnitVerifier()
+        : this(ImmutableStack<string>.Empty) { }
 
-    public void True([DoesNotReturnIf(false)] bool assert, string? message = null) =>
-        AsyncHelper.RunSync(async () => await Assert.That(assert).IsTrue());
+    private TUnitVerifier(ImmutableStack<string> context) => _context = context;
 
-    public void False([DoesNotReturnIf(true)] bool assert, string? message = null) =>
-        AsyncHelper.RunSync(async () => await Assert.That(assert).IsFalse());
+    public void Empty<T>(string collectionName, IEnumerable<T> collection)
+    {
+        if (collection.Any())
+            Assert.Fail(CreateMessage($"'{collectionName}' is not empty"));
+    }
+
+    public void Equal<T>(T expected, T actual, string? message = null)
+    {
+        if (!EqualityComparer<T>.Default.Equals(expected, actual))
+            Assert.Fail(
+                CreateMessage(
+                    message ?? $"items not equal.  expected:'{expected}' actual:'{actual}'"
+                )
+            );
+    }
+
+    public void True([DoesNotReturnIf(false)] bool assert, string? message = null)
+    {
+        if (!assert)
+            Assert.Fail(CreateMessage(message ?? "Expected value to be 'true' but was 'false'"));
+    }
+
+    public void False([DoesNotReturnIf(true)] bool assert, string? message = null)
+    {
+        if (assert)
+            Assert.Fail(CreateMessage(message ?? "Expected value to be 'false' but was 'true'"));
+    }
 
     [DoesNotReturn]
     public void Fail(string? message = null)
     {
-        Assert.Fail(message ?? "Verification failed.");
+        Assert.Fail(CreateMessage(message ?? "Verification failed for an unspecified reason."));
         throw new InvalidOperationException("Unreachable");
     }
 
-    public void LanguageIsSupported(string language) =>
-        AsyncHelper.RunSync(async () =>
-            await Assert.That(language).IsEqualTo(LanguageNames.CSharp)
-        );
+    public void LanguageIsSupported(string language)
+    {
+        if (language != LanguageNames.CSharp && language != LanguageNames.VisualBasic)
+            Assert.Fail(CreateMessage($"Unsupported Language: '{language}'"));
+    }
 
-    public void NotEmpty<T>(string collectionName, IEnumerable<T> collection) =>
-        AsyncHelper.RunSync(async () => await Assert.That(collection).IsNotEmpty());
+    public void NotEmpty<T>(string collectionName, IEnumerable<T> collection)
+    {
+        if (!collection.Any())
+            Assert.Fail(CreateMessage($"'{collectionName}' is empty"));
+    }
 
     public void SequenceEqual<T>(
         IEnumerable<T> expected,
         IEnumerable<T> actual,
         IEqualityComparer<T>? equalityComparer = null,
         string? message = null
-    ) =>
-        AsyncHelper.RunSync(async () =>
-            await Assert
-                .That(
-                    actual.SequenceEqual(expected, equalityComparer ?? EqualityComparer<T>.Default)
-                )
-                .IsTrue()
+    )
+    {
+        var areEqual = actual.SequenceEqual(
+            expected,
+            equalityComparer ?? EqualityComparer<T>.Default
         );
 
-    public IVerifier PushContext(string context) => new TUnitVerifier();
+        if (!areEqual)
+            Assert.Fail(CreateMessage(message ?? "Sequences are not equal"));
+    }
+
+    public IVerifier PushContext(string context) => new TUnitVerifier(_context.Push(context));
+
+    private string CreateMessage(string message) =>
+        _context.Aggregate(
+            message,
+            (current, frame) => "Context: " + frame + Environment.NewLine + current
+        );
 }
